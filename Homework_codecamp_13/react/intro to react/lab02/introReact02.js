73a7896d58eab48ee6ced4f204527ef9cee3fd87
Ohm0025 <115564254+Ohm0025@users.noreply.github.com>
2023-01-08 03:00:16 +0700

    Delete Homework_codecamp_13/react directory

--- Homework_codecamp_13/react/intro to react/lab02/introReact02.js
+++
@@ -1,9 +0,0 @@
-const element = (
-  <div>
-    <h1>Hello JSX</h1>
-    <p>Welcome to react page by JSX</p>
-  </div>
-);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
